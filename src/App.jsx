a82f37284dabf14cc2e7bfc0dcaddfad1dc77350
Sultankhan Pathan <89242322+Sul1871p@users.noreply.github.com>
2025-05-22 19:52:24 +0530

    my project got initiated

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Hero from './components/Hero'
+
+function App() {
+
+  return (
+    <>
+     <p className='text-8xl'>Hi this is sultan</p>
+     <Hero />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <>
+        <div className='flex flex-col items-center justify-center h-screen bg-gray-100'>
+            <h1 className='text-8xl font-bold text-gray-800'>Hi this is Sultan</h1>
+            <p className='mt-4 text-lg text-gray-600'>Welcome to my website!</p>
+        </div>
+    </>
+  )
+}
+
+export default Hero
